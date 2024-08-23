4157d5889530380eb250a08bdc22132697a9fd10
Renan Almeida <renanvictoralmeidasilva@gmail.com>
2024-08-22 21:50:17 -0300

    organizando-pastas-projeto

--- src/main/java/com/RPGSheetManager/app/RPGSheetManager.java
+++ src/main/java/com/RPGSheetManager/app/RPGSheetManager.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package rpgsheetmanager;
+package main.java.com.RPGSheetManager.app;
 
 /**
  *
@@ -16,5 +16,5 @@ public class RPGSheetManager {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
